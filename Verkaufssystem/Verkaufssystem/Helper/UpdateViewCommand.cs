73bd4892884e93f56855fc75096c822ccceb5e8f
moebius-tim <tf.moebius@web.de>
2022-09-20 09:32:38 +0200

    fuegt Fenster und Verzweigungen hinzu

---
+++ Verkaufssystem/Verkaufssystem/Helper/UpdateViewCommand.cs
@@ -0,0 +1,34 @@
+﻿using Verkaufssystem.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Verkaufssystem.Helper
+{
+    public class UpdateViewCommand : ICommand
+    {
+        MainViewModel _viewModel;
+        public event EventHandler CanExecuteChanged;
+
+        public UpdateViewCommand(MainViewModel mvm)
+        {
+            _viewModel = mvm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Home")
+                _viewModel.CurrentViewModel = new HomeViewModel();
+            else if (parameter.ToString() == "List")
+                _viewModel.CurrentViewModel = new ListViewModel();
+        }
+    }
+}
--- Verkaufssystem/Verkaufssystem/ViewModel/MainViewModel.cs
+++ Verkaufssystem/Verkaufssystem/ViewModel/MainViewModel.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Verkaufssystem.Helper;
+using Verkaufssystem.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Verkaufssystem.ViewModel
 {
-    class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        BaseViewModel _currentViewModel = new HomeViewModel();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+
+        public BaseViewModel CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentViewModel"));
+            }
+        }
     }
 }
